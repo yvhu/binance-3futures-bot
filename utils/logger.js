aad898f75261914fc75059c33033de0efb31d740
chunyu.hu <chunyu.hu-vendor@yumchina.com>
2025-07-10 15:37:10 +0800

    feat:更新日志时间问题

--- utils/logger.js
+++ utils/logger.js
@@ -1,4 +1,6 @@
+const moment = require('moment-timezone');
 function log(...args) {
-  console.log(`[${new Date().toISOString()}]`, ...args);
+  // console.log(`[${new Date().toISOString()}]`, ...args);
+  console.log(`[${moment().tz('Asia/Shanghai').format('YYYY-MM-DD HH:mm:ss.SSS')}]`, ...args);
 }
 module.exports = { log };
