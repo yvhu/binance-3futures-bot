065f64e109e2c1dc90e89f69153abfa6453a49df
chunyu.hu <chunyu.hu-vendor@yumchina.com>
2025-08-06 12:52:50 +0800

    .

--- binance/trade.js
+++ binance/trade.js
@@ -112,7 +112,7 @@ async function setLeverage(symbol, leverage) {
   const params = new URLSearchParams({
     symbol,
     leverage: leverage.toString(),
-    timestamp: timestamp.toString()
+    timestamp: localTime.toString()
   });
   const signature = crypto
     .createHmac('sha256', config.binance.apiSecret.trim()) // 同样trim()处理
