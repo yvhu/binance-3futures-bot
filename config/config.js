76da644208aa651fa093bebaf3990b4a35c7b7f8
chunyu.hu <chunyu.hu-vendor@yumchina.com>
2025-08-18 21:46:11 +0800

    .

--- config/config.js
+++ config/config.js
@@ -51,7 +51,7 @@ module.exports = {
 
   takeSelectRunTimeRanges: [
     // 注释掉的时间不执行
-    { start: "00:00", end: "01:00" },
+    // { start: "00:00", end: "01:00" },
     { start: "01:00", end: "02:00" },
     { start: "02:00", end: "03:00" },
     { start: "03:00", end: "04:00" },
@@ -59,14 +59,14 @@ module.exports = {
     { start: "05:00", end: "06:00" },
     { start: "06:00", end: "07:00" },
     { start: "07:00", end: "08:00" },
-    { start: "08:00", end: "09:00" },
+    // { start: "08:00", end: "09:00" },
     { start: "09:00", end: "10:00" },
     { start: "10:00", end: "11:00" },
     { start: "11:00", end: "12:00" },
-    { start: "12:00", end: "13:00" },
+    // { start: "12:00", end: "13:00" },
     { start: "13:00", end: "14:00" },
     { start: "14:00", end: "15:00" },
-    { start: "15:00", end: "16:00" },
+    // { start: "15:00", end: "16:00" },
     { start: "16:00", end: "17:00" },
     { start: "17:00", end: "18:00" },
     { start: "18:00", end: "19:00" },
